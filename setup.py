cdd34b6b15940b55320b1457a1741db2408dbe99
Brian Clough <bclough84@gmail.com>
2026-07-06 09:53:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 from setuptools import setup, find_packages
-setup(name="bayes-jnr", version="0.1.0", packages=find_packages())
+setup(
+    name="bayes-jnr",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=["numpy>=1.18.0", "scipy>=1.4.0"],
+)
