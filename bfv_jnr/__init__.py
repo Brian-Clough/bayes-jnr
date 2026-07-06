b659961e6c0231b2e97a5d61257d04288c6829d6
Brian Clough <bclough84@gmail.com>
2026-07-06 09:33:45 -0500

    Update __init__.py
    update init.py to handle new classes

--- bfv_jnr/__init__.py
+++ bfv_jnr/__init__.py
@@ -1,2 +1,8 @@
+# bfv_jnr/__init__.py
 from bfv_jnr.engine import JNRScaleReconciliationEngine
-__all__ = ["JNRScaleReconciliationEngine"]
+from bfv_jnr.risk_engine import JNRBoundedRiskEngine
+from bfv_jnr.prep import JNRWorkflowPreparer
+from bfv_jnr.finance import DMRVFinancialTranslationEngine
+
+__version__ = "0.1.1"
+__all__ = ["JNRScaleReconciliationEngine", "JNRBoundedRiskEngine", "JNRWorkflowPreparer", "DMRVFinancialTranslationEngine"]
